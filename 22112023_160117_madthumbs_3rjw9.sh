5bdda1443f25933f21c9bcec4a218e2938d2c2c5
tonimontanali <eleonoranesta030@gmail.com>
2023-11-22 16:01:17 +0000

    Fixing madthumbs

---
+++ 22112023_160117_madthumbs_3rjw9.sh
@@ -0,0 +1,1 @@
+In 1986, a drunk fan got into a pace car at Talladega Superspeedway, and started joyriding on the track in front of a nationwide audience. The police cruisers who had to chase him around the track were not amused when they caught and arrested him
