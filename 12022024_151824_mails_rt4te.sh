65c73a9b9405889c3c5ddc89b17150ea193274e0
gangsterino <karinashengelia0@gmail.com>
2024-02-12 15:18:24 +0000

    Changing mails

---
+++ 12022024_151824_mails_rt4te.sh
@@ -0,0 +1,1 @@
+The Uape Indians, who live in the Amazon, mix the ashes of their recently cremated relatives with alcohol, then all members of the family drink the mix with fond memories of the deceased
