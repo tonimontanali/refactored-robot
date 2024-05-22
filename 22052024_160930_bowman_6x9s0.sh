712226e3fd86a61afb60d6bb8b174ed54ab74e3e
tonimontanali <eleonoranesta030@gmail.com>
2024-05-22 16:09:30 +0000

    Updating bowman

---
+++ 22052024_160930_bowman_6x9s0.sh
@@ -0,0 +1,1 @@
+The reason firehouses have circular stairways is from the days when the engines were pulled by horses. The horses were stabled on the ground floor and figured out how to walk up straight staircases
