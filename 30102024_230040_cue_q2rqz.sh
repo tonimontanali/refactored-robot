c05ba66fb4fa2fbf53496f2135113421fa19a75d
tonimontanali <eleonoranesta030@gmail.com>
2024-10-30 23:00:40 +0000

    Fixing cue

---
+++ 30102024_230040_cue_q2rqz.sh
@@ -0,0 +1,1 @@
+The book "Little Red Riding Hood" was banned in 1990 by two school districts in California. They did this because in the book there was a picture of a basket that had a bottle of wine in it
